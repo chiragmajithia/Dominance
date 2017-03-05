5b5ebee49448c95cbbad184dfa758985b31126bb
chirag <chiragamajithia@gmail.com>
2017-03-05 17:33:46 -0500

    Add Initializer Window

--- init.cpp
+++ init.cpp
@@ -12,3 +12,75 @@ Init::~Init()
 {
     delete ui;
 }
+
+void Init::on_pb_commit_clicked()
+{
+    /**
+     * Fix the size of board and  Number of Players
+     * Ask to Add Player Names
+    **/
+    if(ui->pb_commit->isChecked())
+    {
+        qDebug() << "Game Params Fixed ";
+        ui->gb_game_params->setEnabled(false);
+        X_dim = ui->sb_X_dim->value();
+        Y_dim = ui->sb_Y_dim->value();
+        N_players = ui->sb_N_players->value();
+        populatePlayerList();
+        ui->gb_players->setEnabled(true);
+        ui->pb_commit->setText("Reset");
+        ui->pb_start->setEnabled(true);
+    }
+    else
+    {
+        /** Delete Items from Scroll Area **/
+        qDebug() << "Reset Game Params ";
+        resetPlayerList();
+        ui->gb_game_params->setEnabled(true);
+        ui->gb_players->setEnabled(false);
+        ui->pb_commit->setText("Commit");
+        ui->pb_start->setEnabled(false);
+    }
+}
+
+void Init::populatePlayerList()
+{
+    /** Populates Scroll Area with Editable Player Names **/
+       QWidget *central = new QWidget;
+       QVBoxLayout *layout = new QVBoxLayout(central);
+       ui->sa_players->setWidget(central);
+       ui->sa_players->setWidgetResizable(true);
+       led.resize(N_players);
+       for(int i = 0; i < N_players; i++)
+       {
+           led[i] = new QLineEdit(QString("Player %0").arg(i+1));
+           layout->addWidget(led[i]);
+       }
+       layout->addStretch();
+}
+
+void Init::resetPlayerList()
+{
+    /** Free all widgets inside scroll area **/
+    QWidget *central = ui->sa_players->widget();
+        if(central)
+            central->deleteLater();
+   qDebug() << "List View Items removed";
+}
+
+
+void Init::on_pb_start_clicked()
+{
+    /**
+     * Saves the Players Names in QStringList with mappings to Player id
+     * Launches BoardUI with Player Names.
+     **/
+    QStringList players;
+    for(int  i = 0; i < N_players; i++)
+    {
+        players.append(led[i]->text());
+    }
+
+    qDebug() <<"starting board with " <<  players;
+    //hide();
+}
--- init.h
+++ init.h
@@ -2,6 +2,9 @@
 #define INIT_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QLineEdit>
+#include <QVector>
 
 namespace Ui {
 class Init;
@@ -15,8 +18,29 @@ public:
     explicit Init(QWidget *parent = 0);
     ~Init();
 
+private slots:
+
+    void on_pb_commit_clicked();
+    void on_pb_start_clicked();
+
 private:
+
+    /**
+     * Game Parameters
+     * TODO : Set the max and min values of the UI using this constants
+    **/
+    static const int N_MAX = 6, X_MAX = 16, Y_MAX = 16;
+
+    int X_dim, Y_dim, N_players;
+
+    /** UI Stuff  declarations **/
     Ui::Init *ui;
+    QVector<QLineEdit*> led;
+
+    /** Function Declarations **/
+    void populatePlayerList();
+    void resetPlayerList();
+
 };
 
 #endif // INIT_H
